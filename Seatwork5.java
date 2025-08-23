9559e92eab646bb83ca27daa40186f06cba12353
jhanarryaquino <jhanarryaquino08@gmail.com>
2025-08-23 13:46:21 +0000

    hello

--- Seatwork5.java
+++ Seatwork5.java
@@ -1,1 +1,11 @@
+public class Seatwork5 {
+        public static void main(String[] args) {
+                int countdown = 3;
+                        while (countdown > 0) {
+                                    System.out.println(countdown);
+                                                countdown--;
+                                                        }
+                                                                System.out.println("Happy New Year!!");
+                                                                    }
+                                                                  }                                                                  }
 
