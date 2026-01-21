22ba3cf2ef248fa19f8bc014b36a088d1251ae01
jhanarryaquino <jhanarryaquino08@gmail.com>
2026-01-21 14:25:48 +0800

    Create Seatwork4part1.java

---
+++ Seatwork4part1.java
@@ -0,0 +1,15 @@
+public class Sw4part1 {
+    public void processPayment(double amount, String currency) {
+        System.out.println("Processing payment of " + amount + " " + currency);
+    }
+
+    public void processPayment(double amount, String currency, String paymentMethod) {
+        System.out.println("Processing payment of " + amount + " " + currency + " via " + paymentMethod);
+    }
+//main class below
+    public static void main(String[] args) {
+        Sw4part1 processor = new Sw4part1();
+        processor.processPayment(100.0, "ETH");
+        processor.processPayment(250.0, "Vbucks", "Robux"); 
+    }
+}
