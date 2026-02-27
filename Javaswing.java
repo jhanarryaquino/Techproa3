1ca9135e02d9f34c07220b65d06a1e3a3b696a3f
jhanarryaquino <jhanarryaquino08@gmail.com>
2026-02-28 01:54:04 +0800

    Create Javaswing.java

---
+++ Javaswing.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+public class Example1 {
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame("Test1");
+        frame.setSize(400, 300);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+}
